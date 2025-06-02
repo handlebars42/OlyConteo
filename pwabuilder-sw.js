b7d322e0667f01856b90ddf69168ab86dfca34b0
handlebars42 <handlebars42@proton.me>
2025-06-02 14:17:49 -0600

    Update pwabuilder-sw.js (OlyInven rename)

--- pwabuilder-sw.js
+++ pwabuilder-sw.js
@@ -1,21 +1,21 @@
 // Change this to your repository name
-var GHPATH = '/OlyConteo';
+var GHPATH = '/OlyInven';
  
 // Choose a different app prefix name
-var APP_PREFIX = 'olyconteo_';
+var APP_PREFIX = 'olyinven_';
  
 // The version of the cache. Every time you change any of the files
 // you need to change this version (version_01, version_02…). 
 // If you don't change the version, the service worker will give your
 // users the old files!
-var VERSION = 'version_02';
+var VERSION = 'version_03';
  
 // The files to make available for offline use. make sure to add 
 // others to this list
 var URLS = [    
   `${GHPATH}/`,
   `${GHPATH}/index.html`,
-  `${GHPATH}/css/olyinven.css`,
+  `${GHPATH}/css/style.css`,
   `${GHPATH}/controller/barcode.js`,
   `${GHPATH}/controller/main.js`,
   `${GHPATH}/lib/quagga.min.js`,
